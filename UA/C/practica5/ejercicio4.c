8b148d128c9530feac9291b827b408c162a17d7d
Julián Hinojosa Gil <julianhinojosagil@Julians-MacBook-Air.local>
2023-11-06 11:49:15 +0100

    added title to practica5 code

--- UA/C/practica5/ejercicio4.c
+++ UA/C/practica5/ejercicio4.c
@@ -1,3 +1,9 @@
+/*
+            PRÁCTICA 5. EJERCICIO 4.
+    Para compilar: cc ejercicio2.c -o ejercicio2
+    Para ejecutar: ./ejercicio2
+    { cc = compilador }
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
