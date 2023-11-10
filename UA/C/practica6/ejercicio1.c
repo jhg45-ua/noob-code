035cb7a23aff75833c7c3bb96f6c1facc82946ad
Julián Hinojosa Gil <julianhinojosagil@Julians-MacBook-Air.local>
2023-11-10 15:36:34 +0100

    Added recursive divisor

--- UA/C/practica6/ejercicio1.c
+++ UA/C/practica6/ejercicio1.c
@@ -1,30 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void divisores(int n);
+// void divisores(int n);
+//
+// int main() {
+//
+//   int num;
+//
+//   printf("Introduce un numero entero positivo: ");
+//   scanf("%d", &num);
+//
+//   divisores(num);
+//
+//   return 0;
+// }
+//
+// void divisores(int num) {
+//
+//   int nd = 1;
+//
+//   printf("Divisores: 1");
+//   for (int i = 2; i <= num; i++) {
+//     nd = (nd + 1);
+//     if (num % i == 0) {
+//       printf(", %d", i);
+//     }
+//   }
+//   printf("\nNumero de divisores: %d\n", nd / 2);
+// }
+
+void encontrarDivisores(int *numero, int divisores, int *counter);
 
 int main() {
 
-  int num;
+  int numero, counter = 0;
 
-  printf("Introduce un numero entero positivo: ");
-  scanf("%d", &num);
+  printf("Introduce un numero positivo: ");
+  scanf("%d", &numero);
 
-  divisores(num);
+  if (numero < 1) {
+    printf("Por favor introduce un numero positio");
+    return 1;
+  }
+
+  printf("Divisores de %d: ", numero);
+  encontrarDivisores(&numero, 1, &counter);
+  printf("\n");
+
+  printf("Total: %d\n", counter);
 
   return 0;
 }
 
-void divisores(int num) {
+void encontrarDivisores(int *numero, int divisor, int *counter) {
 
-  int nd = 1;
+  if (divisor > *numero)
+    return;
 
-  printf("Divisores: 1");
-  for (int i = 2; i <= num; i++) {
-    nd = (nd + 1);
-    if (num % i == 0) {
-      printf(", %d", i);
-    }
+  if (*numero % divisor == 0) {
+    printf("%d ", divisor);
+    (*counter)++;
   }
-  printf("\nNumero de divisores: %d\n", nd / 2);
+
+  encontrarDivisores(numero, divisor + 1, counter);
 }
