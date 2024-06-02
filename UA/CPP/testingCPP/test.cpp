9e1f5b337ef3886a29cd131c764fb6ca74f57443
ElTitoDG <julianhinojosagil@gmail.com>
2024-06-02 21:55:41 +0200

    test stringstream

--- UA/CPP/testingCPP/test.cpp
+++ UA/CPP/testingCPP/test.cpp
@@ -1,44 +1,25 @@
+//test stringstream
 #include <iostream>
-#include <unistd.h>  // Para getopt
+#include <string>
+#include <sstream>
+#include <vector>
 
-int main(int argc, char* argv[])
+int main()
 {
-    int opt;
-    std::string valorB, valorC;
-    bool opcionA = false;
+    std::vector<std::string> palabras;
 
-    // Procesar las opciones
-    while ((opt = getopt(argc, argv, "ab:c:")) != -1) {
-        switch (opt)
-        {
-            case 'a':
-                opcionA = true;
-                break;
-            case 'b':
-                valorB = optarg;
-                break;
-            case 'c':
-                valorC = optarg;
-                break;
-            default:
-                std::cerr << "Uso: " << argv[0] << " [-a] [-b valor] [-c valor]" << std::endl;
-                return 1;
-        }
+    std::string lista_de_palabras;
+
+    std::cout << "Dime una lista de palabras (separadas por espacios): ";
+    getline(std::cin, lista_de_palabras);
+
+    std::stringstream ss(lista_de_palabras);
+    std::string palabra;
+    while (ss >> palabra)
+    {
+        palabras.push_back(palabra);
     }
 
-    // Mostrar los resultados
-    if (opcionA)
-    {
-        std::cout << "Opción a activada" << std::endl;
-    }
-    if (!valorB.empty())
-    {
-        std::cout << "Opción b con valor: " << valorB << std::endl;
-    }
-    if (!valorC.empty())
-    {
-        std::cout << "Opción c con valor: " << valorC << std::endl;
-    }
-
-    return 0;
+    std::cout << "Hemos leído " << palabras.size() << " palabras" << std::endl;
+    std::cout << "La segunda palabra es: " << palabras[1] << std::endl;
 }
