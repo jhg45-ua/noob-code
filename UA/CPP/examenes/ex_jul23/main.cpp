a63aff62465794f59342071fdcb9fed1b27d5ad9
ElTitoDG <julianhinojosagil@gmail.com>
2024-06-03 08:10:59 +0200

    Comentarios ejercicio julio 23

--- UA/CPP/examenes/ex_jul23/main.cpp
+++ UA/CPP/examenes/ex_jul23/main.cpp
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+// Representación de un trabajador
 struct Employee
 {
     char dni[10];
@@ -17,9 +18,13 @@ struct Employee
     unsigned int bruteSalary;
     unsigned int startedYear;
 };
-
+/* Función: carga los empleados encontrados en un archivo binario
+ * filename: nombre del archivo
+ * vector<Employee> &employees: paso por referencia de un conjunto de empleados
+ * */
 bool loadEmployee(const char *filename, vector<Employee> &employees)
 {
+    // Abrimos archivo binario
     ifstream inputFile(filename, ios::binary);
     if(!inputFile.is_open())
     {
@@ -27,6 +32,7 @@ bool loadEmployee(const char *filename, vector<Employee> &employees)
         return false;
     }
 
+    // Creamos un empleado hasta que no queda nada en el archivo y lo metemos al conjunto
     Employee emp;
     while (inputFile.read(reinterpret_cast<char *>(&emp), sizeof(Employee)))
     {
@@ -36,9 +42,11 @@ bool loadEmployee(const char *filename, vector<Employee> &employees)
     inputFile.close();
     return true;
 }
-
+/* Función: calcula el salario medio dado un conjunto de empleados
+ * */
 double calculateAverageSalary(vector<Employee> &employees)
 {
+    // Recorremos el vector donde en cada iteración vamos sumando el total de salarios
     double totalSalary = 0;
     for (int i = 0; i < employees.size(); i++)
     {
@@ -48,24 +56,30 @@ double calculateAverageSalary(vector<Employee> &employees)
     return totalSalary / employees.size();
 }
 
+/* Función: creamos un archivo donde vamos metiendo a los empleados que cumplen
+ * las condiciones para teletrabajar
+ *
+ * */
 void writeAptEmployee(const char *filename, const vector<Employee> &employees, double averageSalary)
 {
+    // Creamos el archivo con el nombre filename de forma normal (no binaria)
     ofstream outputFile(filename);
     if (!outputFile.is_open())
     {
         cerr << "Error: Cannot open file!" << endl;
     }
 
+    // Inicializamos un ID para ordenar a los empleados
     int id = 1;
     for (int i = 0; i < employees.size(); i++)
     {
+        // Tiene que cumplir estar más de 3 años desde 2023 y tener un salario bruto mayor al medio
         if (2023 - employees[i].startedYear > 3 && employees[i].bruteSalary >= averageSalary)
         {
             outputFile << id << "-" << employees[i].dni << endl;
             id++;
         }
     }
-
     outputFile.close();
 }
 
@@ -74,10 +88,13 @@ int main(int argc, char *argv[])
     const char *inputFilename = nullptr;
     double averageSalary = -1;
 
+    // Recorremos argumentos
     for (int i = 1; i < argc; i++)
     {
+        // Vemos si coincide
         if (strcmp(argv[i], "-b") == 0)
         {
+            // Vemos si tiene algo después
             if (i + 1 < argc)
             {
                 inputFilename = argv[i + 1];
@@ -89,12 +106,14 @@ int main(int argc, char *argv[])
                 return 1;
             }
         }
+
+        // Lo mismo con el siguiente
         else if (strcmp(argv[i], "-s") == 0)
         {
             if (i + 1 < argc)
             {
                 averageSalary = atof(argv[i + 1]);
-                if (averageSalary < 10000 || averageSalary > 10000)
+                if (averageSalary < 10000 || averageSalary > 100000)
                 {
                     cerr << "Error: wrong arguments" << endl;
                     return 1;
@@ -107,6 +126,7 @@ int main(int argc, char *argv[])
                 return 1;
             }
         }
+
         else
         {
             cerr << "Error: wrong arguments" << endl;
