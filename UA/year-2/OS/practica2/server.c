d4d96fcc3dd829f3577178b0ece60cc84f4391b9
Julian Hinojosa Gil <julianhinojosagil@gmail.com>
2025-11-02 18:26:32 +0000

    Update server.c

--- UA/year-2/OS/practica2/server.c
+++ UA/year-2/OS/practica2/server.c
@@ -1,10 +1,11 @@
 #include <netinet/in.h> //structure for storing address information
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+// #include <string.h>
 #include <sys/socket.h> //for socket APIs
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char const* argv[])
 {
@@ -20,7 +21,7 @@ int main(int argc, char const* argv[])
         fprintf(stderr, "Error al crear el socket");
         exit(EXIT_FAILURE);
     }
-    fprintf(stdout, "Socket creado con exito\n");
+    printf("Socket creado con exito\n");
 
     serverAddr.sin_port = htons(9999);
     serverAddr.sin_family = AF_INET;
@@ -34,23 +35,32 @@ int main(int argc, char const* argv[])
     listen(sockfd, 5);
 
     while(1) {
+
+        printf("Esperando nuevas conexiones...\n");
+
+        size = sizeof(clientAddr);
+        newSocketfd = accept(sockfd, (struct sockaddr *)&clientAddr, &size);
+        if (newSocketfd == -1) {
+            fprintf(stderr, "Error aceptando la conexion\n");
+            continue;
+        }
+
+        printf("Conexion aceptada!!!\n");
+
         pid = fork();
 
         if (pid == 0) {
-            printf("El hijo espera nuevas conexiones...\n");
-            size = sizeof(clientAddr);
-            newSocketfd = accept(sockfd, (struct sockaddr *)&clientAddr, &size);
-            if (newSocketfd == -1) {
-                fprintf(stderr, "Error aceptando la conexion");
-                exit(EXIT_FAILURE);
-            }
-            printf("El hijo acepta la conexion...\n");
-
-            sleep(30);
             close(sockfd);
+
+            printf("El hijo maneja la conexion...\n");
+
+            close(newSocketfd);
+            exit(EXIT_SUCCESS);
+        } else if (pid > 0) {
+            close(newSocketfd);
         } else {
-            printf("Esperando a que acabe el hijo\n");
-            wait(NULL);
+            fprintf(stderr, "Error al crear el proceso hijo");
+            close(newSocketfd);
         }
     }
 
